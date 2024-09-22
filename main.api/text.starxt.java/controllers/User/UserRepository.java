d903c84483401f30232bd499e38355a19e80ba6b
Abhijit Patil <157373320+AbhiFutureTech@users.noreply.github.com>
2024-09-22 10:16:23 +0530

    Update UserRepository.java

--- main.api/text.starxt.java/controllers/User/UserRepository.java
+++ main.api/text.starxt.java/controllers/User/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * The interface User repository.
+ * The interface User repository
  *
  * @author Givantha Kalansuriya
  */
